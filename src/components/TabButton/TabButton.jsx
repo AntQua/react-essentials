0761478d8cd56b1a9b6bbdef5ef77f6c05e17e8d
Antonio Quaresma <tozequa@gmail.com>
2024-03-02 11:50:42 +0000

    Building Section component and forwarding Props to wrapped elements

--- src/components/TabButton/TabButton.jsx
+++ src/components/TabButton/TabButton.jsx
@@ -1,10 +1,13 @@
 /* eslint-disable react/prop-types */
 import './TabButton.css';
 
-export default function TabButton({ children, onSelect, isSelected }) {
-    return (
-      <li>
-        <button  className={isSelected ? 'active' : undefined} onClick={onSelect}>{children}</button>
-      </li>
-    );
-  }
+export default function TabButton({ children, isSelected, ...props }) {
+
+  return (
+    <li>
+      <button className={isSelected ? 'active' : undefined} {...props}>
+        {children}
+      </button>
+    </li>
+  );
+}
